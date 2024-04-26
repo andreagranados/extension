1365e2ee662ce6c07b3cadf9d84261b8c0f76f8d
andreagranados <andrea.granados@fi.uncoma.edu.ar>
2024-04-26 12:21:52 -0300

    Datos->Servicios Web Accesibles: rest_localhost

---
+++ metadatos/tablas/apex_servicio_web.sql
@@ -0,0 +1,12 @@
+
+------------------------------------------------------------
+-- apex_servicio_web
+------------------------------------------------------------
+INSERT INTO apex_servicio_web (proyecto, servicio_web, descripcion, tipo, param_to, param_wsa) VALUES (
+	'extension', --proyecto
+	'rest_localhost', --servicio_web
+	NULL, --descripcion
+	'rest', --tipo
+	NULL, --param_to
+	'0'  --param_wsa
+);

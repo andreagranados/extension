c8206d99fdfcd074876114a14cd9b1da78ef54a4
andreagranados <andrea.granados@fi.uncoma.edu.ar>
2024-05-06 10:18:40 -0300

    dt_organizaciones_participantes: comento get_listado_filtro

--- php/datos/dt_organizaciones_participantes.php
+++ php/datos/dt_organizaciones_participantes.php
@@ -44,40 +44,41 @@ class dt_organizaciones_participantes extends extension_datos_tabla {
             }
         }
     }
-    function get_listado_filtro($id, $filtro = array()) {
-
-        $where = array();
-        if (isset($filtro)) {
-            $where[] = $filtro;
-        }
-        $sql = "SELECT
-                    o_p.id_organizacion ,
-                    o_p.id_tipo_organizacion ,
-                    o_p.id_pext ,
-                    o_p.nombre ,
-                    o_p.id_localidad ,
-                    o_p.telefono ,
-                    o_p.email ,
-                    o_p.referencia_vinculacion_inst,
-                    o_p.id_pais,
-                    o_p.id_provincia,
-                    o_p.domicilio,
-                    o_p.aval
-                    
-                FROM
-                   organizaciones_participantes as o_p INNER JOIN pextension as p_e ON (o_p.id_pext = p_e.id_pext)"
-                . "LEFT OUTER JOIN (SELECT l.id,l.localidad FROM dblink('" . $this->dblink_designa() . "','SELECT id,localidad  FROM localidad') as l (id INTEGER, localidad CHARACTER VARYING(255) )) as l"
-                . " ON (o_p.id_localidad = l.id AND o_p.localidad = l.localidad)
-                   LEFT OUTER JOIN tipo_organizacion as t_o ON (o_p.id_tipo_organizacion = t_o.id_tipo_organizacion)
-                    where o_p.localidad = l.localidad"
-
-        ;
-        if (count($where) > 0) {
-            $sql = sql_concatenar_where($sql, $where)
-                    . "AND o_p.id_pext=" . $id;
-        }
-        return toba::db('extension')->consultar($sql);
-    }
+    //no se llama desde ningun lado
+//    function get_listado_filtro($id, $filtro = array()) {
+//
+//        $where = array();
+//        if (isset($filtro)) {
+//            $where[] = $filtro;
+//        }
+//        $sql = "SELECT
+//                    o_p.id_organizacion ,
+//                    o_p.id_tipo_organizacion ,
+//                    o_p.id_pext ,
+//                    o_p.nombre ,
+//                    o_p.id_localidad ,
+//                    o_p.telefono ,
+//                    o_p.email ,
+//                    o_p.referencia_vinculacion_inst,
+//                    o_p.id_pais,
+//                    o_p.id_provincia,
+//                    o_p.domicilio,
+//                    o_p.aval
+//                    
+//                FROM
+//                   organizaciones_participantes as o_p INNER JOIN pextension as p_e ON (o_p.id_pext = p_e.id_pext)"
+//                . "LEFT OUTER JOIN (SELECT l.id,l.localidad FROM dblink('" . $this->dblink_designa() . "','SELECT id,localidad  FROM localidad') as l (id INTEGER, localidad CHARACTER VARYING(255) )) as l"
+//                . " ON (o_p.id_localidad = l.id AND o_p.localidad = l.localidad)
+//                   LEFT OUTER JOIN tipo_organizacion as t_o ON (o_p.id_tipo_organizacion = t_o.id_tipo_organizacion)
+//                    where o_p.localidad = l.localidad"
+//
+//        ;
+//        if (count($where) > 0) {
+//            $sql = sql_concatenar_where($sql, $where)
+//                    . "AND o_p.id_pext=" . $id;
+//        }
+//        return toba::db('extension')->consultar($sql);
+//    }
 
 //    function get_listado($id = null) {
 //        $sql = "SELECT
